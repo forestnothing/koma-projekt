62edafe58f2188029274b487c201d1f0b0747388
forestnothing <74041134+forestnothing@users.noreply.github.com>
2020-11-18 15:43:37 +0100

    Added navigation options

--- App.js
+++ App.js
@@ -3,76 +3,77 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {
-  Image,
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+    Image,
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
+    Header,
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Floors from './Components/Floors.js'
 import HomeScreen from './Components/HomeScreen.js'
 import Exhibitions from './Components/Exhibitions.js'
-import Events from'./Components/Events.js'
+import Events from './Components/Events.js'
 import VisitorInfo from './Components/VisitorInfo.js'
 import Map from './Components/Map.js'
-
+import ExhibitInfo from './Components/ExhibitInfo.js'
 
 
 
 const Stack = createStackNavigator();
 const App = () => {
-  return (
+    return (
 
-    <NavigationContainer>
+        <NavigationContainer>
 
-<Stack.Navigator
-screenOptions={
-  
-  {
+            <Stack.Navigator
+                screenOptions={
 
- 
-  
-
-  headerStyle: {
-    backgroundColor: '#252525',
-   
-    
-  
-  },
-  headerTintColor: '#fff',
-  headerTitleStyle: {
-    fontWeight: 'bold',
-    
-  },
-
-  
-}}>
-  <Stack.Screen name="Home" component={HomeScreen} options={{title: 'Startsida' }}/>
-  <Stack.Screen name="Floors" component={Floors} options={{title: 'Välj en våning nedan'}}/>
-  <Stack.Screen name="Exhibitions" component={Exhibitions} options={{title: 'Utställningar'}}/>
-  <Stack.Screen name="Events" component={Events} options={{title: 'Evenemang'}}/>
-  <Stack.Screen name="VisitorInfo" component={VisitorInfo} options={{title: 'Besöksinfo'}}/>
-  <Stack.Screen name="Map" component={Map} options={{title: 'Karta över utställningar'}}/>
-</Stack.Navigator>
-    </NavigationContainer>
+                    {
 
 
 
-   
-  
-  );
+
+                        headerStyle: {
+                            backgroundColor: '#252525',
+
+
+
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+
+                        },
+
+
+                    }}>
+                <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Startsida' }} />
+                <Stack.Screen name="Floors" component={Floors} options={{ title: 'Välj en våning nedan' }} />
+                <Stack.Screen name="Exhibitions" component={Exhibitions} options={{ title: 'Utställningar' }} />
+                <Stack.Screen name="Events" component={Events} options={{ title: 'Evenemang' }} />
+                <Stack.Screen name="VisitorInfo" component={VisitorInfo} options={{ title: 'Besöksinfo' }} />
+                <Stack.Screen name="Map" component={Map} options={{ title: 'Karta över utställningar' }} />
+                <Stack.Screen name="ExhibitInfo" component={ExhibitInfo} options={{ title: 'Information om utställning' }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+
+
+
+
+
+    );
 };
 
 
